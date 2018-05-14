a5f25b2281221af9f8cd7e81edf4f8d618a6b0db
Suvedh Srikanth <suvedhs@gmail.com>
2018-05-14 02:37:39 -0500

    retrieved all messages where changes are needed

--- content.js
+++ content.js
@@ -3,11 +3,22 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.messagey === "clicked_browser_action" ) {
-      console.log("howddy");
-      var messageSpan = $("div.clearfix div._3058 div._aok span._3oh-").eq(0);
-      console.log(messageSpan.text());
 
-      alert("Howdy!\n" + messageSpan.text());
+      //verifies that the website is messenger.com
+      if(window.location.href.indexOf("messenger.com") > -1) {
+
+        //gets number of loaded text-based messages
+        var a = $("div.clearfix div._3058 div._aok span._3oh-").length;
+
+        //outputs each message to the console
+        for(i = 1; i < a + 1; i++) {
+          console.log($("div.clearfix div._3058 div._aok span._3oh-").eq(-i).text() + "\n");
+        }
+      }
+      else {
+        console.log("oops");
+      }
+
       //messageSpan.text("hi");
 //      console.log(messageSending);
 
