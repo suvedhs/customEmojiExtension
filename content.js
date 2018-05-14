f99695950e16e5497de351aa9d78c319addfdcda
Suvedh Srikanth <suvedhs@gmail.com>
2018-05-14 01:08:43 -0500

    acquired the span in messenger.com

--- content.js
+++ content.js
@@ -3,11 +3,17 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.messagey === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
+      if(1 === 0) {
+        var messageSending = $(".4rv3 div._5irm._1_73 div._kmc._2swd.navigationFocus div._3-8x._3oh- div div._5rp7._5rp8 div._5rpb div.notranslate._5rpu div div div._1mf._1mj span span").attr("innerHTML");
+        console.log("welp the if statement works");
+      }
+      console.log("howddy");
+      var messageSpan = $("div._1mf._1mj span span").eq(0);
+      console.log(messageSpan.text());
 
-      console.log(firstHref);
-
-      chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+      alert("Howdy!\n" + messageSpan.text());
+      //messageSpan.text("hi");
+//      console.log(messageSending);
 
     }
   }
