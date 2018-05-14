b9b59d46fc78bee4eabd952718aed8a2b75cae9c
Suvedh Srikanth <suvedhs@gmail.com>
2018-05-14 01:53:33 -0500

    changed the target message

--- content.js
+++ content.js
@@ -3,12 +3,8 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.messagey === "clicked_browser_action" ) {
-      if(1 === 0) {
-        var messageSending = $(".4rv3 div._5irm._1_73 div._kmc._2swd.navigationFocus div._3-8x._3oh- div div._5rp7._5rp8 div._5rpb div.notranslate._5rpu div div div._1mf._1mj span span").attr("innerHTML");
-        console.log("welp the if statement works");
-      }
       console.log("howddy");
-      var messageSpan = $("div._1mf._1mj span span").eq(0);
+      var messageSpan = $("div.clearfix div._3058 div._aok span._3oh-").eq(0);
       console.log(messageSpan.text());
 
       alert("Howdy!\n" + messageSpan.text());
